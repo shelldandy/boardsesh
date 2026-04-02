3d7790f7453dce8c59288b59ef4adb1aa1526be3
Marco de Jongh <mdejongh@atlassian.com>
2026-04-02 19:25:05 +1100

    Lock iOS app to portrait

--- mobile/ios/App/App/BoardseshViewController.swift
+++ mobile/ios/App/App/BoardseshViewController.swift
@@ -8,4 +8,16 @@ class BoardseshViewController: CAPBridgeViewController {
         // Disable rubber-band bounce so the page cannot overscroll
         webView?.scrollView.bounces = false
     }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        .portrait
+    }
+
+    override var preferredInterfaceOrientationForPresentation: UIInterfaceOrientation {
+        .portrait
+    }
+
+    override var shouldAutorotate: Bool {
+        false
+    }
 }
